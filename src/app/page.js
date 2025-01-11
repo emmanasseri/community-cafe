fd52e131d9ebc8b69aca7307717370d6d7aab7dc
emmanasseri <enasseri02@gmail.com>
2025-01-11 16:34:22 -0600

    styling changes

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-5xl md:text-7xl font-bold">community café</h1>
-      <p className="text-lg text-gray-400 mt-4">
-        a third space for the decentrally-minded.
+    <main className="flex flex-col items-center  min-h-screen pt-16  circular-gradient">
+      <h1 className="text-9xl md:text-[8rem] font-bold text-center">
+        community café
+      </h1>
+      <p className="text-xl md:text-2xl text-gray-100 mt-6 text-center">
+        an IRL 3rd space for the decentrally-minded.
       </p>
 
       <Image
-        src="/pixel-latte.png"
+        src="/welcome-latte.gif"
         alt="Community Cafe"
         width={300}
         height={200}
         className="m-8"
         priority
       />
+      <section className="flex flex-col md:flex-row items-center justify-between px-12 pt-16 bg-gray-800">
+        {/* Filler Text */}
+        <div className="md:w-1/2 text-gray-200">
+          <h2 className="text-4xl md:text-6xl font-bold mb-4">
+            About Community Café
+          </h2>
+          <p className="text-lg md:text-xl">
+            Community Café is a decentralization-focused, community owned third
+            space focused on making people's lives better through healthy food,
+            community, and creativity.
+          </p>
+        </div>
+
+        {/* Image on the Right */}
+        <div className="md:w-1/2 flex justify-center mt-8 md:mt-0">
+          <Image
+            src="/pixel-people.png"
+            alt="People at Community Cafe"
+            width={500}
+            height={300}
+            className="rounded-lg"
+            priority
+          />
+        </div>
+      </section>
     </main>
   );
 }
